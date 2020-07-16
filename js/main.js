31cded620aa9de1e1d5e87a9c33389505647fc4f
Mandar A <mandarashtekar@users.noreply.github.com>
2020-07-15 18:42:19 -0700

    Update main.js
    Before Install Event check-in

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 window.onload = () => {
-  'use strict';
+  	'use strict';
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js');
-  }
+  	if ('serviceWorker' in navigator) {
+    	navigator.serviceWorker.register('./sw.js');
+  	}
+
+  	window.addEventListener('beforeinstallprompt', (event) => {
+		console.log('👍', 'beforeinstallprompt', event);
+		// Stash the event so it can be triggered later.
+		window.deferredPrompt = event;
+		// Remove the 'hidden' class from the install button container
+		divInstall.classList.toggle('hidden', false);
+	});
 };
