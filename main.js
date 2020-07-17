f7ab3022536dfbc4e1b1c141d6155b25e30a279e
Mandar A <mandarashtekar@users.noreply.github.com>
2020-07-16 19:12:49 -0700

    Update main.js
    Bug Fix

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ window.onload = () => {
 };
 
 /* *************** Push Notification - START *************** */
-var butInstall = document.queryselector('butInstall');
+var butInstall = document.querySelector('butInstall');
 
 // Push Notifications for PWA
 butInstall.addEventListener('click', () => {
