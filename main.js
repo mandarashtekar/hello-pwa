6b38303e691905662c91013c6ed8edaa18c97cba
Mandar A <mandarashtekar@users.noreply.github.com>
2020-07-16 18:48:18 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ window.onload = () => {
   	'use strict';
 
   	if ('serviceWorker' in navigator) {
-    	navigator.serviceWorker.register('/sw.js')
+    	navigator.serviceWorker.register('sw.js')
           .then(function (registration){
             console.log('Service worker registered successfully on scope: ' +registration.scope);
           }).catch(function(e){
